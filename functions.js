6fe087e5916db45a41c50ec8ffd4561a4ce24a45
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-07-31 22:11:22 +0530

    Add additional functions for number handling, scoring, and age checking

--- functions.js
+++ functions.js
@@ -18,3 +18,23 @@ const isPositive = (num) => {
 }
 console.log(isPositive(10));
 console.log(isPositive(-5)); 
+
+function number(...val) {
+    console.log(val);  
+}
+number(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+function getScore(...scores) {
+    let total = 0;
+    scores.forEach(function(score) {
+        total += score;
+    });
+    return total;
+}
+console.log(getScore(10, 20, 30, 40, 50));
+
+function checkAge(age){
+    if (age <18) return "Too young";
+    return "Allowed";
+}
+console.log(checkAge(16));
