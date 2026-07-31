a964dd11f009d19780f33232315fd0f134d1faeb
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-07-31 22:48:27 +0530

    Add discountCalculator function and update console logs for age checking

--- functions.js
+++ functions.js
@@ -37,4 +37,15 @@ function checkAge(age){
     if (age <18) return "Too young";
     return "Allowed";
 }
-console.log(checkAge(16)); 
+console.log(checkAge(16));
+
+
+function discountCalculator(discount) {
+    return function(price) {
+        return price - (price * discount / 100);
+    }
+}
+let ten = discountCalculator(10);
+let twenty = discountCalculator(20);
+console.log(ten(1200));
+console.log(twenty(1200));
