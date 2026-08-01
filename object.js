177e35b02aa68f105c655a5513782e3ac333a0ba
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-08-01 21:39:29 +0530

    Add object.js with initial object definitions and console logs for book, employee, student, and obj

---
+++ object.js
@@ -0,0 +1,38 @@
+let book = {
+    title: "JavaScript",
+    price: 500
+}
+console.log(book.title);
+
+//***********************************************************************************
+
+let employee = {
+    name: "Rahul",
+    salary: 50000
+}
+
+let key = "salary";
+
+console.log(employee[key]);
+
+//***********************************************************************************
+
+let student = {
+    name: "Mohammad",
+    course: "BCA"
+}
+
+student.city = "Delhi";
+
+console.log(student);
+
+//***********************************************************************************
+
+let obj = {
+    brand: "Apple",
+    price: 80000
+}
+
+obj.price = 90000
+
+console.log(obj);
