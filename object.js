bf3eb589cb77cf22346bc9e417227350f3ffb3a9
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-08-02 22:08:01 +0530

    Add multiple calls to object.increase() to increment the counter

--- object.js
+++ object.js
@@ -329,6 +329,8 @@ let object = {
     }
 };
 object.increase();
+object.increase();
+object.increase();
 console.log(object.counter);
 
 //***********************************************************************************
