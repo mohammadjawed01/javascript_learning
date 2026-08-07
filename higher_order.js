fab9f0cef29cff9e534a7f1a6efcc42b6a674c92
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-08-07 21:50:02 +0530

    Add for-of loop examples to higher_order.js

---
+++ higher_order.js
@@ -0,0 +1,44 @@
+//forOf loop ---------------------------------------------------------------------
+
+let fruits = ["Apple", "Banana", "Mango"];
+
+for (let fruit of fruits) {
+    console.log(fruit);
+}
+
+// ---------------------------------------------------------------------------
+
+let numbers = [10, 20, 30];
+
+for (let number of numbers) {
+    console.log(number);
+}
+
+// ---------------------------------------------------------------------------
+
+let numbers = [10, 20, 30];
+
+let sum = 0;
+
+for (let number of numbers) {
+    sum += number;
+}
+
+console.log(sum);
+
+// ---------------------------------------------------------------------------
+
+let numbers = [2, 4, 6, 8, 10];
+for (let number of numbers) {
+    if (number>5) {
+        console.log(number);
+    }
+}
+
+// ---------------------------------------------------------------------------
+
+ let learn = ["HTML", "CSS", "JavaScript"];
+for (let subject of learn) {
+    console.log(I an learning ${subject});
+
+// ---------------------------------------------------------------------------
