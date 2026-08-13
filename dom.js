db0bd1c00107d1a5c68f19cdd9392b3841ed9344
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-08-13 22:23:58 +0530

    Add dom.js for DOM manipulation examples and update index.html to include the script

---
+++ dom.js
@@ -0,0 +1,18 @@
+let heading = document.querySelector("h1");
+console.log(heading);
+
+let message = document.querySelector(".message");
+console.log(message);
+
+let title = document.querySelector("#title");
+console.log(title.textContent); 
+
+let button = document.querySelector("button");
+console.log(button);
+
+let para = document.querySelector("p");
+console.log(para.textContent); 
+
+let x = document.querySelector("#title");
+console.log(x);
+console.log(x.textContent);
