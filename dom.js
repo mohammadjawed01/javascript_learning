fc17fda897a2b63905fe58de5dc4f2b360ffc55a
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-08-13 23:06:28 +0530

    Add query selectors and corresponding HTML elements for DOM manipulation examples

--- dom.js
+++ dom.js
@@ -1,18 +1,24 @@
+// 1.
 let heading = document.querySelector("h1");
 console.log(heading);
 
+// 2.
 let message = document.querySelector(".message");
 console.log(message);
 
+// 3.
 let title = document.querySelector("#title");
 console.log(title.textContent); 
 
+// 4.
 let button = document.querySelector("button");
 console.log(button);
 
+// 5.
 let para = document.querySelector("p");
 console.log(para.textContent); 
 
+// 6.
 let x = document.querySelector("#title");
 console.log(x);
 console.log(x.textContent);
