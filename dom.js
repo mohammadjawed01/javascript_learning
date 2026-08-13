c71a98faf1524e90770c2ec174678c616f2cfbba
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-08-13 23:37:41 +0530

    Add new HTML elements for DOM manipulation examples in index.html

--- dom.js
+++ dom.js
@@ -1,6 +1,6 @@
 // 1.
-let heading = document.querySelector("h1");
-console.log(heading);
+// let heading = document.querySelector("h1");
+// console.log(heading);
 
 // 2.
 let message = document.querySelector(".message");
@@ -22,3 +22,23 @@ console.log(para.textContent);
 let x = document.querySelector("#title");
 console.log(x);
 console.log(x.textContent);
+
+// 7.
+let titles = document.getElementById("title1");
+console.log(titles.textContent);
+
+// 8.
+let messages = document.querySelectorAll("p");
+console.log(messages);
+
+// 9.
+let item = document.getElementsByClassName("item");
+console.log(item[1].textContent);
+
+// 10.
+let headings = document.getElementsByTagName("h2");
+console.log(headings[2].textContent);
+
+// 11.
+let paragraph = document.querySelector("p");
+paragraph.textContent = "first paragraph"
