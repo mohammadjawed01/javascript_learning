2068fbe79aa77d11e354448ee44b91cf409746e7
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-08-19 20:14:02 +0530

    Add initial HTML and JavaScript files for DOM manipulation examples

---
+++ dom/dom2.js
@@ -0,0 +1,28 @@
+// let id = document.getElementById("title");
+// console.log(id);
+
+// 2
+// let id = document.getElementById("title");
+// id.textContent = "New text";
+// console.log(id);
+
+// 3
+// let id = document.getElementById("box");
+// id.innerHTML = "<h2>New Heading</h2>"
+// console.log(id)
+
+// 4
+// let id = document.getElementById("title");
+// id.style.color = "red"
+// id.style.backgroundColor = "black"
+// id.style.fontSize = "18px"
+
+// console.log(id);
+
+// 5
+// let img = document.getElementById("photo");
+// img.getAttribute("src")
+
+
+
+
