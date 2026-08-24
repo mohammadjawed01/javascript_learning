239e8b6dd8c738c8d10d63be4a1603e1ca2f7e0b
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-08-24 20:10:55 +0530

    add some question for learn the event in this learn only what is event

--- events.js
+++ events.js
@@ -1,7 +1,19 @@
+// let btn = document.querySelector("#btn");
+// let page = document.querySelector("#page")
+// btn.addEventListener("click", function () {
+//     page.classList.toggle("dark")
+//     console.log(btn)
+//     console.log(page)
+// })
+
+// 1
+// let btn = document.querySelector("#btn");
+// btn.addEventListener('click', function(){
+//     alert("button clicked!")
+// })
+
+// 2
 let btn = document.querySelector("#btn");
-let page = document.querySelector("#page")
-btn.addEventListener("click", function(){
-    page.classList.toggle("dark")
-    console.log(btn)
-    console.log(page)
+btn.addEventListener('click', function(){
+    console.log("Button Clicked")
 })
