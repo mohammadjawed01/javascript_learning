065fd412cae7883ae4db581ab962f26333a02cb5
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-08-27 20:40:11 +0530

    add new files for practice the real world exapmle of events and remove the practice project code from the event.html and event.js and add into this files

--- events.js
+++ events.js
@@ -462,23 +462,3 @@ document.addEventListener("keydown", function (event) {
 //     console.log(event.clientX)
 //     console.log(event.clientY)
 // })
-
-// ----------------------simple pratice-----------------------
-// 1
-// let sec = document.querySelector("select");
-// let heading = document.querySelector(".heading");
-// sec.addEventListener('change', function(event){
-//     heading.textContent = `${event.target.value} device selected`
-//     // console.log(event.target.value)
-// })
-
-// 2
-let h2 = document.querySelector('h2')
-window.addEventListener('keydown', function(event){
-    if(event.key === " "){
-        h2.textContent = "SPC"
-    }
-    else{
-        h2.textContent = event.key
-    }
-})
