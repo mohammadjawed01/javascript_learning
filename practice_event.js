2c7efc43dfed4c705ef27648d0d043b1c3a1e717
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-08-27 21:18:12 +0530

    make a selector file button they select the file from the user

--- practice_event.js
+++ practice_event.js
@@ -8,12 +8,28 @@
 // })
 
 // 2
-let h2 = document.querySelector('h2')
-window.addEventListener('keydown', function(event){
-    if(event.key === " "){
-        h2.textContent = "SPC"
+// let h2 = document.querySelector('h2')
+// window.addEventListener('keydown', function(event){
+//     if(event.key === " "){
+//         h2.textContent = "SPC"
+//     }
+//     else{
+//         h2.textContent = event.key
+//     }
+// })
+
+// 3
+let btn = document.querySelector("#btn");
+let fileinp = document.querySelector("#fileinp");
+btn.addEventListener('click', function(){
+    fileinp.click();
+})
+fileinp.addEventListener('change', function(event){
+    let file = event.target.files[0]
+    if(file){
+        btn.textContent = file.name;
     }
-    else{
-        h2.textContent = event.key
+    else {
+        btn.textContent = "Upload file";
     }
 })
