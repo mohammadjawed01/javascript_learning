9a92d0b5428bdc0db99f33b09fda870bc7ca2cd0
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-08-27 20:27:13 +0530

    add practice project in a simple practice section to learn more events

--- events.js
+++ events.js
@@ -465,9 +465,20 @@ document.addEventListener("keydown", function (event) {
 
 // ----------------------simple pratice-----------------------
 // 1
-let sec = document.querySelector("select");
-let heading = document.querySelector(".heading");
-sec.addEventListener('change', function(event){
-    heading.textContent = `${event.target.value} device selected`
-    // console.log(event.target.value)
+// let sec = document.querySelector("select");
+// let heading = document.querySelector(".heading");
+// sec.addEventListener('change', function(event){
+//     heading.textContent = `${event.target.value} device selected`
+//     // console.log(event.target.value)
+// })
+
+// 2
+let h2 = document.querySelector('h2')
+window.addEventListener('keydown', function(event){
+    if(event.key === " "){
+        h2.textContent = "SPC"
+    }
+    else{
+        h2.textContent = event.key
+    }
 })
