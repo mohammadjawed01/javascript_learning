bf6ac6f02d63a505d6bc57e4c6020f28d7a0538d
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-09-01 21:55:01 +0530

    add new html and javascript file for practice the form control and add checkbox for terms agreement and event listener for change

---
+++ form-validation/form_control.js
@@ -0,0 +1,9 @@
+// 
+// let terms = document.querySelector("#terms");
+// console.log(terms.checked);
+
+// 
+let terms = document.querySelector("#terms");
+terms.addEventListener('change', ()=> {
+    console.log(terms.checked);
+})
