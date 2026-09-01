178e6b4464b453f910ff1dc5768affe3dab2498d
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-09-01 21:55:21 +0530

    remove commented-out checkbox code and clean up radio button selection logic

--- form-validation/form_control.js
+++ form-validation/form_control.js
@@ -3,7 +3,18 @@
 // console.log(terms.checked);
 
 // 
-let terms = document.querySelector("#terms");
-terms.addEventListener('change', ()=> {
-    console.log(terms.checked);
-})
+// let terms = document.querySelector("#terms");
+// terms.addEventListener('change', ()=> {
+//     console.log(terms.checked);
+// })
+
+// RADIO BUTTON
+// let male = document.querySelector("#male");
+// console.log(male.checked)
+
+let selected = document.querySelector('input[name="gender"]:checked');
+if (selected) {
+    console.log(selected.value);
+}
+
+
