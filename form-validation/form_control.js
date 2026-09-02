c5f8c6b8047b43eda16d4f84532403404a4de7ce
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-09-02 08:40:09 +0530

    feat: add radio button selection for plan and form submission validation

--- form-validation/form_control.js
+++ form-validation/form_control.js
@@ -18,12 +18,49 @@
 // }
 
 // SELECT/DROPDOWN
-let city = document.querySelector("#city");
-// console.log(city.value)
-// let selectedOption = city.options[city.selectedIndex];
-// console.log(selectedOption);
-city.addEventListener('change', function(e){
-    console.log(city.value)
+// let city = document.querySelector("#city");
+// // console.log(city.value)
+// // let selectedOption = city.options[city.selectedIndex];
+// // console.log(selectedOption);
+// city.addEventListener('change', function(e){
+//     console.log(city.value)
+// })
+
+
+// CHECKBOX + FORM EXAMPLE
+// let form = document.querySelector("form");
+// let username = document.querySelector("#username");
+// let terms = document.querySelector("#terms");
+
+// form.addEventListener('submit', function(e){
+//     e.preventDefault();
+
+//     if(username.value === ""){
+//         alert("plz fill the username")
+//         return
+//     }
+
+//     if(!terms.checked){
+//         alert("PLease accept the terms");
+//         return;
+//     }
+//     alert("Registratioin Successful");
+// })
+
+// RADIO + FORM EXAMPLE
+
+let form = document.querySelector("form");
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    let selected = document.querySelector('input[name="plan"]:checked');
+
+    if (!selected) {
+        alert("please select a plan");
+        return
+    }
+
+    alert("selected value")
 })
 
 
