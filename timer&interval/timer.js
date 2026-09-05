6fc9d3f2aaeb696e09c5de37b2563831c43936f7
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-09-05 20:58:48 +0530

    add a dowloading simulation bar with the help of html, css and javaScript.

--- timer&interval/timer.js
+++ timer&interval/timer.js
@@ -15,13 +15,29 @@
 // clearInterval(interval);
 
 // counter
-let count = 10;
-let int = setInterval(function(){
-    if(count>=0){
-        console.log(count)
-        count--;
+// let count = 10;
+// let int = setInterval(function(){
+//     if(count>=0){
+//         console.log(count)
+//         count--;
+//     }
+//     else{
+//         clearInterval(int);
+//     }
+// },2000)
+
+// download bar
+let progress = document.querySelector(".progress-bar")
+let percent = document.querySelector(".percent")
+let count = 0;
+let intv = setInterval(function () {
+    if (count < 100) {
+        count++;
+        progress.style.width = `${count}%`
+        percent.textContent = `${count}%`
     }
     else{
-        clearInterval(int);
+        document.querySelector("h2").textContent = "Downloaded.";
+        clearInterval(intv)
     }
-},2000)
+}, 1000 / 10)
