7e49a2dfcf64b6f650fcedcd28223f350f0be4aa
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-09-05 20:06:23 +0530

    add a simple counter for practice

--- timer&interval/timer.js
+++ timer&interval/timer.js
@@ -7,9 +7,21 @@
 // clearTimeout(timeout)
 
 //setinterval
-let interval = setInterval(function(){
-    alert("hey")
-},3000);
+// let interval = setInterval(function(){
+//     alert("hey")
+// },3000);
 
 // clearinterval
-clearInterval(interval);
+// clearInterval(interval);
+
+// counter
+let count = 10;
+let int = setInterval(function(){
+    if(count>=0){
+        console.log(count)
+        count--;
+    }
+    else{
+        clearInterval(int);
+    }
+},2000)
