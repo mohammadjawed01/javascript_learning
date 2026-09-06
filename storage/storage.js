0f5e68eba86afef55909d235b2e7efec5e48541a
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-09-06 20:51:47 +0530

    convert the localstorage string value to number

--- storage/storage.js
+++ storage/storage.js
@@ -1,7 +1,14 @@
 // local storage
 localStorage.setItem("username", "jawed");
-console.log(localStorage.getItem("username"));
+// console.log(localStorage.getItem("username"));
 //remove the item 
 localStorage.removeItem("username")
 //clear the local storage
 localStorage.clear();
+
+// 
+localStorage.setItem("age", 20);
+console.log(localStorage.getItem("age"));   //localStorage give a string value 
+//if we wnat a number than we do 
+let age = Number(localStorage.getItem("age"));
+console.log(typeof age)
