71f2a551e7141485fe4c3056fd9da81069312191
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-09-16 20:43:38 +0530

    add colorChanger.html and colorChanger.js for background color changing functionality with start and stop buttons

---
+++ timer&interval/colorChanger.js
@@ -0,0 +1,24 @@
+let changeBgColor = function () {
+    let hex = "0123456789ABCDEF";
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+let intervalId;
+let startChangingColor = function () {
+    intervalId = setInterval(chnageRandomColor, 1000);
+    function chnageRandomColor() {
+        document.body.style.backgroundColor = changeBgColor();
+    }
+}
+let stopChangingColor = function () {
+    clearInterval(intervalId);
+}
+
+document.querySelector("#start").addEventListener('click', startChangingColor);
+
+document.querySelector("#stop").addEventListener('click', stopChangingColor);
