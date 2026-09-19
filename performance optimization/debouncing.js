cc94cc4ca13ca6e34a3806302a554281a2f9df72
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-09-19 21:17:11 +0530

    rename the folder from debouncing to performance optimization add: create debouncing and throttling HTML and JS files

---
+++ performance optimization/debouncing.js
@@ -0,0 +1,16 @@
+let input = document.querySelector("input");
+
+function debouncing(fnc, delay) {
+    let timer;
+    return function (...args) {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            fnc(...args);
+        }, delay)
+    };
+}
+
+input.addEventListener('input', debouncing(function () {
+    console.log("hey");
+}, 1000)
+);
