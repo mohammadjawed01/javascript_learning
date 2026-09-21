eee8fd965f2ab5812953655223459eb80221f933
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-09-21 21:14:49 +0530

    fix: correct addition logic in button click event and improve variable naming

--- sepration and concern/script.js
+++ sepration and concern/script.js
@@ -1,15 +1,19 @@
 let btn = document.querySelector("button");
 let ul = document.querySelector("ul");
 
+
+function add(n1, n2){
+    return n1 + n2;
+}
 btn.addEventListener('click', function () {
     let numb1 = Math.floor(Math.random() * 10)
     let numb2 = Math.floor(Math.random() * 10)
 
-    let add = numb1 + numb2;
+    let sum = add(numb1, numb2);
 
     let li = document.createElement("li");
 
-    li.textContent = add;
+    li.textContent = sum;
 
     ul.appendChild(li);
 })
